778da920b1dc1e74b180d4511e719931e4cfb304
Ryan Doner <>
2019-03-07 16:44:56 -0700

    clone. Change photos. Work on updating resume

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,11 +1,23 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 class About extends Component {
   render() {
-    return(
-      <div><h1>About Page</h1></div>
-    )
+    return (
+      <div>
+        <h1>I AM A...</h1>
+        <p className="about-me">
+          ...fullstack web developer from Tucson, AZ who currently resides in Boulder, Colorado. In
+          2015, I graduated from The University of Arizona with a BS in Neuroscience and Cognitive
+          Sciences. After college, I explored a few different career paths while teaching myself web
+          development. In 2018, I completed a 6 month web developer apprenticeship program with the
+          Techtonic Group in Boulder. For our classroom project I was the team lead for an app built
+          using the MERN stack and I later worked on a React Native app for a client. I'm passionate
+          about learning new things which I express through my passions for web development, rock
+          climbing and scuba diving. I'm currently looking for a full time position as a web
+          developer in the Boulder-Denver Metro area.
+        </p>
+      </div>
+    );
   }
 }
 
--- src/components/resume.js
+++ src/components/resume.js
@@ -1,97 +1,108 @@
-import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
-import Education from './education';
-import Experience from './experience';
-import Skills from './skills';
-
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+import Education from "./education";
+import Experience from "./experience";
+import Skills from "./skills";
 
 class Resume extends Component {
   render() {
-    return(
+    return (
       <div>
         <Grid>
           <Cell col={4}>
-            <div style={{textAlign: 'center'}}>
+            <div style={{ textAlign: "center" }}>
               <img
-                src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+                src="flatironsPortrait.jpg"
                 alt="avatar"
-                style={{height: '200px'}}
-                 />
+                style={{ height: "200px", borderRadius: "50%" }}
+              />
             </div>
 
-            <h2 style={{paddingTop: '2em'}}>Paul Hanna</h2>
-            <h4 style={{color: 'grey'}}>Programmer</h4>
-            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+            <h2 style={{ paddingTop: "2em" }}>Ryan Doner</h2>
+            <h4 style={{ color: "grey" }}>React Developer</h4>
+            <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
+            <p>
+              Motivated fullstack web developer with a strong desire to pursue excellence and
+              continuously improve everyday. Looking to join a collaborative team made up of
+              intellectually curious individuals who share a passion for personal development,
+              mastering their craft, and who strive for balanced lives.
+            </p>
+            <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
             <h5>Address</h5>
-            <p>1 Hacker Way Menlo Park, 94025</p>
+            <p>3100 22nd St. Boulder, CO</p>
             <h5>Phone</h5>
-            <p>(123) 456-7890</p>
+            <p>(520) 664-6294</p>
             <h5>Email</h5>
-            <p>someone@example.com</p>
-            <h5>Web</h5>
-            <p>mywebsite.com</p>
-            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+            <p>ryanelliotdoner@gmail.com</p>
+            <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
           </Cell>
           <Cell className="resume-right-col" col={8}>
             <h2>Education</h2>
 
-
             <Education
-              startYear={2002}
-              endYear={2006}
-              schoolName="My University"
-              schoolDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-               />
+              startYear={2011}
+              endYear={2015}
+              schoolName="University of Arizona"
+              schoolDescription={
+                "Bachelor of Science: Neuroscience and Cognitive Sciences, 2015\nUniversity of Arizona - Tucson\nGPA: 3.78\nGraduate of UofAs McGuire entrepreneurship program"
+              }
+            >
+              <li>Bachelor of Science: Neuroscience and Cognitive Sciences</li>
+              <li>GPA: 3.78</li>
+              <li>Graduate of UofA's McGuire entrepreneurship program</li>
+            </Education>
 
-               <Education
-                 startYear={2007}
-                 endYear={2009}
-                 schoolName="My 2nd University"
-                 schoolDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-                  />
-                <hr style={{borderTop: '3px solid #e22947'}} />
+            <Education startYear={2018} endYear={2019} schoolName="Techtonic Academy">
+              <li>Completed rigorous 6 month web development apprenticeship</li>
+              <li>Team lead for a chore coordination app built using the MERN stack</li>
+            </Education>
 
-              <h2>Experience</h2>
+            <hr style={{ borderTop: "3px solid #e22947" }} />
+
+            <h2>Experience</h2>
 
             <Experience
-              startYear={2009}
-              endYear={2012}
-              jobName="First Job"
+              startYear={2017}
+              endYear={2018}
+              jobName="RM3 Labs"
               jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-              />
+            >
+              <li>
+                Designed and tested Labware LIMS (Lab Information Management System) to assist in
+                automation of tasks including data analysis and reporting.
+              </li>
+              <li>
+                Crafted lab report templates using SAP Crystal Reports software utilizing both SQL
+                and Excel databases.
+              </li>
+              <li>
+                Programmed LIMS Basic (derivative of Visual Basic) scripts to make calculations from
+                data in SQL databases.
+              </li>
+              <li>
+                Wrote documentation for LIMS software specialized to chemists, biologists, and
+                secretarial staff.
+              </li>
+            </Experience>
 
-              <Experience
-                startYear={2012}
-                endYear={2016}
-                jobName="Second Job"
-                jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-                />
-              <hr style={{borderTop: '3px solid #e22947'}} />
-              <h2>Skills</h2>
-              <Skills
-                skill="javascript"
-                progress={100}
-                />
-                <Skills
-                  skill="HTML/CSS"
-                  progress={80}
-                  />
-                  <Skills
-                    skill="NodeJS"
-                    progress={50}
-                    />
-                    <Skills
-                      skill="React"
-                      progress={25}
-                      />
-
-
+            <Experience
+              startYear={2019}
+              endYear={2018}
+              jobName="Techtonic"
+              jobDescription="Software Developer"
+            >
+              <li>Constructed a React-Native front end for a waterpark company</li>
+            </Experience>
+            <hr style={{ borderTop: "3px solid #e22947" }} />
+            <h2>Skills</h2>
+            <Skills skill="javascript" progress={100} />
+            <Skills skill="HTML/CSS" progress={80} />
+            <Skills skill="NodeJS" progress={50} />
+            <Skills skill="React" progress={25} />
           </Cell>
         </Grid>
       </div>
-    )
+    );
   }
 }
 
