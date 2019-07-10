834e37703ad9a12936662b0f6d758d89136fa502
rdoner <ryanelliotdoner@gmail.com>
2019-07-10 09:07:25 -0600

    revert dependency updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
-import Main from './components/main';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import "./App.css";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+import Main from "./components/main";
+// import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 // import flatironsPortrait from 'flatironsPortrait.jpg';
 
 class App extends Component {
@@ -10,38 +12,53 @@ class App extends Component {
     return (
       <div className="demo-big-content">
         <Layout className="layout">
-            <Header 
-              className="header-color" 
-              style={{
-                // TODO: Customize background color
-              backgroundColor: '#5b7baf', 
-              boxShadow: 'inset 0px -40px 58px -37px #000000'}}
-              title={
-                // TODO: Customize font family
-                <Link className="home-button" style={{textDecoration: 'none', color: 'white', fontFamily: 'Source Code Pro'}} 
-                  to="/">Home
-                </Link>} 
-              scroll>
-                <Navigation>
-                    <Link to="/resume">Resume</Link>
-                    <Link to="/aboutme">About Me</Link>
-                    <Link to="/projects">Projects</Link>
-                </Navigation>
-            </Header>
-            <Drawer title={<Link style={{textDecoration: 'none', color: 'black', fontFamily: 'Source Code Pro'}} to="/">Home</Link>}>
-                <Navigation>
-                  <Link to="/resume">Resume</Link>
-                  <Link to="/aboutme">About Me</Link>
-                  <Link to="/projects">Projects</Link>
-                </Navigation>
-            </Drawer>
-            <Content>
-                <div className="page-content" />
-                <Main/>
-            </Content>
+          <Header
+            className="header-color"
+            style={{
+              // TODO: Customize background color
+              backgroundColor: "#5b7baf",
+              boxShadow: "inset 0px -40px 58px -37px #000000"
+            }}
+            title={
+              // TODO: Customize font family
+              <Link
+                className="home-button"
+                style={{ textDecoration: "none", color: "white", fontFamily: "Source Code Pro" }}
+                to="/"
+              >
+                Home
+              </Link>
+            }
+            scroll
+          >
+            <Navigation>
+              <Link to="/resume">Resume</Link>
+              <Link to="/aboutme">About Me</Link>
+              <Link to="/projects">Projects</Link>
+            </Navigation>
+          </Header>
+          <Drawer
+            title={
+              <Link
+                style={{ textDecoration: "none", color: "black", fontFamily: "Source Code Pro" }}
+                to="/"
+              >
+                Home
+              </Link>
+            }
+          >
+            <Navigation>
+              <Link to="/resume">Resume</Link>
+              <Link to="/aboutme">About Me</Link>
+              <Link to="/projects">Projects</Link>
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className="page-content" />
+            <Main />
+          </Content>
         </Layout>
       </div>
-
     );
   }
 }
